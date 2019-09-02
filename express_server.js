fbe74ca984bfd39dc65e122a3a785bb36d3b8f60
Gary Chen <garychen.whc@gmail.com>
2019-09-02 23:06:32 +0000

    delete button

--- express_server.js
+++ express_server.js
@@ -52,6 +52,11 @@ app.post("/urls", (req, res) => {
   }
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
 
 app.get("/u/:shortURL", (req, res) => {
   if (req.params.shortURL === "undefined") {
